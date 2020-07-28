1e059b5615bd8090565e6d93bdb1a3f33eab7ad5
John A. M <j_a_mcmenemy@yahoo.com>
2020-07-28 13:16:04 +0100

    Added some test data for the naming constests aand basic React component looping to display it. Next gonna work on SASS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import data from './testData.json';
+
 import App from './components/App';
 
-ReactDOM.render(<App headerMessage="Hello Prop" />, document.getElementById('root'));
+ReactDOM.render(<App contests={data.contests} />, document.getElementById('root'));
